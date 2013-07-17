137b66742ac91f7b74d7d17a6670ce378a04589f
Eldar Zakirov <unixod@gmail.com>
2013-07-17 18:18:47 +0400

    CMake scripts refactoring

---
+++ cmake/showHeadersInIDE.cmake
@@ -0,0 +1,14 @@
+#
+# showHeadersInIDE(file_paths...)
+#
+# Parameters:
+# file_paths... -   files that shall be accessible from IDE
+#
+# Description:
+# Include directory content for convenience in editing in IDE
+#
+function(showHeadersInIDE)
+    string(MD5 TARGET_NAME ${CMAKE_CURRENT_LIST_FILE})
+
+    add_custom_target(${TARGET_NAME} ALL SOURCES ${ARGN})
+endfunction()
--- src/amf/tests/cmake/addTest.cmake
+++ src/amf/tests/cmake/addTest.cmake
@@ -12,9 +12,15 @@
 # and add this binary as test.
 #
 function(addTest test_name file_stem)
-    set(CPP_SOURCE_FILE_NAME "test_${file_stem}.cpp")
-    set(TEST_FILE_NAME "${file_stem}.test")
+    set(CPP_SOURCE_FILE_NAME
+        "test_${file_stem}.cpp")
+
+    set(TEST_FILE_NAME
+        "${file_stem}.test")
+
     add_executable(${TEST_FILE_NAME} ${CPP_SOURCE_FILE_NAME})
+
     target_link_libraries(${TEST_FILE_NAME} ${ARGN})
+
     add_test(${test_name} ${TEST_FILE_NAME})
 endfunction()
