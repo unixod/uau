a1ef3d22f313d25cb16a281ff1ee208494795ab9
Eldar Zakirov <unixod@gmail.com>
2014-07-22 23:02:12 +0400

    Issue 17 - done

--- src/amf/src/core/main_router.cpp
+++
@@ -1,9 +0,0 @@
-#include "core/main_router.h"
-#include "core/abstract_actor_id.h"
-
-namespace amf = uau::amf;
-namespace core = uau::amf::core;
-
-std::pair<std::vector<core::AbstractActor::Id>, bool> core::MainRouter::rout(core::AbstractActor::Id, std::shared_ptr<core::Envelope<>>)
-{
-}
